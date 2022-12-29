218efe8f635537a2c763d36b1a5d232fd4f6535d
Luis Trejo <trejorias@gmail.com>
2022-12-29 12:17:59 -0600

    fix:	multiple entries with same title. 	book-card min width adjusted 	book-card differentiated fields

--- script.js
+++ script.js
@@ -5,13 +5,9 @@ const editForm = document.querySelector('#edit-form')
 const bookContainer = document.querySelector('.book-container')
 const overlayPopUp = document.querySelector('.overlay-popUp')
 
-
 newBook.addEventListener('click', popUpHandler)
-
 closeFormButton.forEach(btn => addEventListener('click', popUpHandler))
-
 bookForm.addEventListener('submit', addBook)
-
 editForm.addEventListener('submit', editBook)
 
 let myLibrary = []
@@ -29,7 +25,13 @@ class Book {
 myLibrary.push(new Book('1984', 'George Orwell', 328, false))
 myLibrary.push(new Book('The Hobbit', 'J.R.R. Tolkien', 310, true))
 myLibrary.push(new Book('Dracula', 'Bram Stoker', 431, true))
+myLibrary.push(new Book('Animal Farm', 'George Orwell', 86, true))
+myLibrary.push(new Book('The Fellowship of The Ring', 'J.R.R. Tolkien', 432, false))
 myLibrary.push(new Book('The Martian', 'Andy Weir', 387, true))
+myLibrary.push(new Book('The Last Wish', 'Andrzej Sapkowski', 384, false))
+myLibrary.push(new Book('Atomic Habits', 'James Clear', 320, true))
+
+renderCards()
 
 function popUpHandler(e) {
   //Handles new book form, and its close button, edit form and its close button
@@ -101,18 +103,16 @@ function createBookCard(book) {
 
   const labelRead = document.createElement('label')
   labelRead.setAttribute('for', `tgl${book.title}`)
-  labelRead.textContent = book.read === true ? 'Read' : 'Not yet'
+  labelRead.textContent = book.read === true ? 'Read' : 'Not yet read'
   pRead.append(toggleRead, labelRead)
 
   const divMenu = document.createElement('div')
   divMenu.classList.add('book-card-menu')
   const imgDel = document.createElement('img')
-  // imgDel.src = './images/delete40.png'
   imgDel.src = './images/trash-outline.svg'
   imgDel.addEventListener('click', deleteCard)
 
   const imgEdit = document.createElement('img')
-  // imgEdit.src = './images/edit40.png'
   imgEdit.src = './images/pencil-outline.svg'
   imgEdit.title = 'edit'
   imgEdit.addEventListener('click', popUpHandler)
@@ -127,32 +127,32 @@ function deleteCard(e) {
   const card = e.target.parentNode.parentNode
   const title = card.firstChild.textContent
 
-  //Delete book from library
-  myLibrary = myLibrary.filter(b => b.title !== title)
-  //Delete card
-  document.querySelector('.book-container').removeChild(card)
+  if (confirm(`Do you want to delete ${title} entry?`)) {
+    //Delete book from library
+    myLibrary = myLibrary.filter(b => b.title !== title)
+    //Delete card
+    document.querySelector('.book-container').removeChild(card)
+  }
 }
 
 function editBook(e) {
   e.preventDefault()
-
   const cardNodes = document.querySelectorAll('.book-card')
   let currentCard = ''
   cardNodes.forEach(bc => {
     if (bc.firstChild.textContent === currentBook.title) currentCard = bc
   })
-
   //Validate duplicate title
   const titles = myLibrary.length > 0
     ? myLibrary.map(b => {
-      if (b.title !== currentCard.firstChild.textContent)
-        return b.title
+      if (b.title.toLowerCase() !== currentCard.firstChild.textContent.toLowerCase())
+        return b.title.toLowerCase()
     })
     : '-'
-  if (titles.includes(document.querySelector('#editTitle').value)) {
+  console.log(titles)
+  if (titles.includes(document.querySelector('#editTitle').value.toLowerCase())) {
     alert('Title already registered')
   } else {
-
     //Setting new values Card and Book
     currentBook.title = document.querySelector('#editTitle').value
     currentBook.author = document.querySelector('#editAuthor').value
@@ -162,12 +162,15 @@ function editBook(e) {
     currentCard.childNodes[1].textContent = currentBook.author
     currentCard.childNodes[2].textContent = currentBook.pages
 
+    //update toggleButton id
+    currentCard.childNodes[3].firstChild.id = `tgl${currentBook.title}`
+    currentCard.childNodes[3].lastChild.setAttribute('for', `tgl${currentBook.title}`)
+
     popUpHandler(e)
   }
 }
 
 function toggleReadHandler(e) {
-
   const currentCard = e.target.parentNode.parentNode.childNodes
   myLibrary.forEach(b => {
     if (b.title === currentCard[0].textContent) currentBook = b
@@ -178,7 +181,7 @@ function toggleReadHandler(e) {
     currentBook.read = true
   } else {
     currentCard[3].firstChild.checked = false
-    currentCard[3].lastChild.textContent = 'Not yet'
+    currentCard[3].lastChild.textContent = 'Not yet read'
     currentBook.read = false
   }
 }
@@ -188,5 +191,3 @@ function renderCards() {
     myLibrary.forEach(book => createBookCard(book))
   }
 }
-
-renderCards()
